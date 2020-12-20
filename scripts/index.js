6a6567136a97d5cd7700eedf9766b01a0b940c4d
Estefanía Cordeiro Brión <estefania.cbrion@gmail.com>
2020-12-20 21:54:38 +0100

    Animate de progress ring done

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,7 @@
 'use strict';
 
-const COLOR_CODES_RING = {
-    info: {
-        color: "green"
-    }
-};
-// Create initial color for remaining time path.
-let remainingPathColor = COLOR_CODES_RING.info.color;
-
 // Declaring variable of interval of time (pomodoro = 25 minutes)
-let time = 25;
+let time = 1;
 // Start with an initial value of 25 minutes
 let timeLimit = time * 60;
 // Initially, no time has passed, but this will count up
@@ -18,14 +10,16 @@ let timePassed = 0;
 let timeLeft = timeLimit;
 let timeInterval = null;
 
-
 /* Basic markup and styles countdown and setting up time label */
+/* Use to animate the length of remaining time line.
+    Ring should cover the full length of the circle:
+    Length = 2πr = 2 * π * 45 = 282,6 ≈ 283 */
 document.getElementById('time__display').innerHTML = `
 <section class="base-time">
     <svg class="base-time__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <g class="base-time__circle">
             <circle class="base-time__path-elapsed" cx="50" cy="50" r="45"></circle>
-            <path id="base-time-path-remaining" class="base-time__path-remaining ${remainingPathColor}"
+            <path id="base-time-path-remaining" stroke-dasharray="283" class="base-time__path-remaining"
             d="
             M 50, 50
             m -45, 0
@@ -43,6 +37,10 @@ document.getElementById('time__display').innerHTML = `
 
 startTime();
 
+function onTimesUp() {
+    clearInterval(timeInterval);
+}
+
 function startTime() {
     timeInterval = setInterval(() => {
         // The amount of time passed increments by one
@@ -50,18 +48,41 @@ function startTime() {
         timeLeft = timeLimit - timePassed;
         // The time left label is updated
         document.getElementById("base-time-label").innerHTML = formatTime(timeLeft);
+        setCircleDasharray();
+        if (timeLeft === 0) {
+            onTimesUp();
+        }
     }, 1000);
 }
 
 function formatTime(time) {
     // The largest round integer less than or equal to the result of time divided being by 60.
-    const minutes = Math.floor(time / 60);
+    let minutes = Math.floor(time / 60);
     // Seconds are the remainder of the time divided by 60 (modulus operator).
     let seconds = time % 60;
-    // If the value of seconds is less than 10, then display seconds with a leading zero.
-    if (seconds < 10) {
-        seconds = `0${seconds}`;
+
+    // If the value of minutes or seconds are less than 10, then display seconds with a leading zero.
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+        if(seconds < 10) {
+            seconds = `0${seconds}`;
+        }
     }
     // The output in MM:SS format.
     return `${minutes}:${seconds}`;
+}
+
+// Divides time left by the defined time limit.
+function calculateTimeFraction() {
+    const rawTimeFraction = timeLeft / timeLimit;
+    return rawTimeFraction - (1 / timeLimit) * (1 - rawTimeFraction);
+}
+
+// Update the dasharray value as time passes, starting with 283
+function setCircleDasharray() {
+    const fullDashArray = 283;
+    const circleDasharray = `${(calculateTimeFraction() * fullDashArray).toFixed(0)} 283`;
+    document
+    .getElementById("base-time-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
 }
